94fea760f60cdc5118ae5b3e462702f1f8de3fed
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-06-30 12:25:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ if (API) {
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
   const imageContainer = document.querySelector('.image-container');
-  const imageUrls = [
-    { word: '사과', url: 'https://cdn.mkhealth.co.kr/news/photo/202010/50970_51164_4758.jpg' },
-    { word: '바나나', url: 'https://health.chosun.com/site/data/img_dir/2022/05/04/2022050401754_0.jpg' },
-    { word: '오렌지', url: 'http://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2016/12/13/20161213000681_0.jpg' },
-    // 다양한 단어와 이미지 URL을 추가합니다.
+  const imageWords = [
+    { word: '사과', imageSrc: 'images/사과.jpg' },
+    { word: '바나나', imageSrc: 'images/바나나.jpg' },
+    { word: '감자', imageSrc: 'images/감자.jpg' }, 
+    { word: '고구마', imageSrc: 'images/고구마.jpg ' },
+    // 다양한 단어와 이미지 경로를 추가합니다.
   ];
 
   let hideTimeout = null;
@@ -28,9 +29,9 @@ if (API) {
       const transcript = result[0].transcript.toLowerCase();
       speechResult.textContent = transcript;
 
-      for (const image of imageUrls) {
-        if (transcript.includes(image.word)) {
-          showImage(image.url);
+      for (const word of imageWords) {
+        if (transcript.includes(word.word)) {
+          showImage(word.imageSrc);
           removeImageBackgroundDelayed();
           break;
         }
@@ -38,19 +39,19 @@ if (API) {
     }
   };
 
-  function showImage(url) {
+  function showImage(imageSrc) {
     clearTimeout(hideTimeout);
     const imageElement = document.createElement('img');
-    imageElement.src = url;
+    imageElement.src = imageSrc;
     imageContainer.appendChild(imageElement);
   }
 
   function removeImageBackgroundDelayed() {
-    clearTimeout(hideTimeout); // 이전에 예약된 사라짐 타이머를 취소합니다.
+    clearTimeout(hideTimeout);
     hideTimeout = setTimeout(() => {
       while (imageContainer.firstChild) {
         imageContainer.removeChild(imageContainer.firstChild);
       }
-    }, 3000); // 3초 후에 그림이 사라지도록 합니다.
+    }, 3000);
   }
 }
