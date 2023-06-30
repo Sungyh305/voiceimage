78f8ceac1b88f3f9dd31bab599bc654566cfbc82
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-06-30 13:28:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -57,8 +57,8 @@ if (API) {
   function showImage(imageSrc) {
     const imageElement = document.createElement('img');
     imageElement.src = imageSrc;
-    imageElement.width = 100; // 이미지의 가로 크기를 설정합니다.
-    imageElement.height = 100; // 이미지의 세로 크기를 설정합니다.
+    imageElement.width = 50; // 이미지의 가로 크기를 설정합니다.
+    imageElement.height = 50; // 이미지의 세로 크기를 설정합니다.
     imageElement.onerror = function () {
       this.style.display = 'none'; // 이미지 로딩에 실패하면 숨깁니다.
     };
