07aaf0a912581b6e393936d47255324526371196
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-06-30 11:19:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ if (API) {
   const recognition = new API();
 
   recognition.continuous = true;
-  recognition.lang = 'ko-GB';
+  recognition.lang = 'ko-KR';
 
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
   const imageContainer = document.querySelector('.image-container');
   const imageUrls = [
-    { word: 'apple', url: 'https://cdn.mkhealth.co.kr/news/photo/202010/50970_51164_4758.jpg' },
-    { word: 'banana', url: 'https://health.chosun.com/site/data/img_dir/2022/05/04/2022050401754_0.jpg' },
-    { word: 'orange', url: 'http://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2016/12/13/20161213000681_0.jpg' },
+    { word: '사과', url: 'https://cdn.mkhealth.co.kr/news/photo/202010/50970_51164_4758.jpg' },
+    { word: '바나나', url: 'https://health.chosun.com/site/data/img_dir/2022/05/04/2022050401754_0.jpg' },
+    { word: '오렌지', url: 'http://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2016/12/13/20161213000681_0.jpg' },
     // 다양한 단어와 이미지 URL을 추가합니다.
   ];
 
