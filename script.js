655537317c37fc312b61f428f6bdf326328cac9e
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-06-30 14:32:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,9 @@ if (API) {
 
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
-  const images = [
-    '고구마.jpg',
-    '감자.jpg',
-    '사과.jpg',
-    '바나나.jpg'
-  ];
+  const imageContainer = document.querySelector('.image-container');
+  const imageUrls = ['고구마.jpg', '감자.jpg', '사과.jpg', '바나나.jpg'];
+  let currentImageIndex = 0;
 
   button.addEventListener('click', () => {
     recognition.start();
@@ -21,26 +18,15 @@ if (API) {
   });
 
   recognition.onresult = (event) => {
-    const spokenText = event.results[event.results.length - 1][0].transcript;
-    speechResult.textContent = spokenText;
+    for (const result of event.results) {
+      speechResult.textContent = result[0].transcript;
 
-    const imageContainer = document.querySelector('.container');
-    imageContainer.innerHTML = '';
-
-    for (const image of images) {
-      if (!image.toLowerCase().includes(spokenText.toLowerCase())) {
-        const imgElement = document.createElement('img');
-        imgElement.src = image;
-        imageContainer.appendChild(imgElement);
+      // 이미지를 삭제하고 새 이미지로 대체함
+      const images = imageContainer.querySelectorAll('img');
+      if (images.length === 2) {
+        images[currentImageIndex].src = imageUrls[currentImageIndex];
+        currentImageIndex = (currentImageIndex + 1) % 4;
       }
-    }
-
-    const remainingImages = imageContainer.querySelectorAll('img');
-
-    if (remainingImages.length === 0) {
-      const noImageText = document.createElement('p');
-      noImageText.textContent = 'No more images';
-      imageContainer.appendChild(noImageText);
     }
   };
 }
