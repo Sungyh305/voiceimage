0cda90b051a4c22093f2abb79f656508419fccc2
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-06-30 14:01:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ if (API) {
 
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
-  const images = document.querySelectorAll('img');
-  const hiddenClass = 'hidden';
+  const container = document.querySelector('.container');
+  const visibleImages = ['고구마.jpg', '감자.jpg'];
+  const hiddenImages = ['사과.jpg', '바나나.jpg'];
+
+  let currentVisibleIndex = 0;
 
   button.addEventListener('click', () => {
     recognition.start();
@@ -18,14 +21,36 @@ if (API) {
 
   recognition.onresult = (event) => {
     const spokenText = event.results[event.results.length - 1][0].transcript;
-    speechResult.textContent = spokenText;
+    speechResult.textContent = spokenText.toLowerCase();
 
-    for (const image of images) {
-      if (image.alt.toLowerCase().includes(spokenText.toLowerCase())) {
-        image.classList.add(hiddenClass);
-      } else {
-        image.classList.remove(hiddenClass);
+    if (spokenText.toLowerCase() === 'next') {
+      currentVisibleIndex++;
+      if (currentVisibleIndex >= visibleImages.length) {
+        currentVisibleIndex = 0;
       }
     }
+
+    updateVisibleImages();
   };
+
+  function updateVisibleImages() {
+    container.innerHTML = '';
+
+    const visibleImageSrc = visibleImages[currentVisibleIndex];
+    const visibleImage = createImageElement(visibleImageSrc);
+    container.appendChild(visibleImage);
+
+    for (const hiddenImageSrc of hiddenImages) {
+      const hiddenImage = createImageElement(hiddenImageSrc);
+      hiddenImage.classList.add('hidden');
+      container.appendChild(hiddenImage);
+    }
+  }
+
+  function createImageElement(src) {
+    const img = document.createElement('img');
+    img.src = 'images/' + src;
+    img.alt = src;
+    return img;
+  }
 }
