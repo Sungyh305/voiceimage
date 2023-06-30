794b76f5ed96b135d021a14bb6c7bf69702dec09
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-06-30 14:15:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ if (API) {
 
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
-  const imageContainer = document.querySelector('.image-container');
   const images = [
     '고구마.jpg',
     '감자.jpg',
@@ -25,21 +24,23 @@ if (API) {
     const spokenText = event.results[event.results.length - 1][0].transcript;
     speechResult.textContent = spokenText;
 
-    const matchingImage = images.find(image => image.toLowerCase().includes(spokenText.toLowerCase()));
-    if (matchingImage) {
-      images.splice(images.indexOf(matchingImage), 1);
-    }
-
+    const imageContainer = document.querySelector('.container');
     imageContainer.innerHTML = '';
 
     for (const image of images) {
-      const imgElement = document.createElement('img');
-      imgElement.src = image;
-      imageContainer.appendChild(imgElement);
+      if (!image.toLowerCase().includes(spokenText.toLowerCase())) {
+        const imgElement = document.createElement('img');
+        imgElement.src = image;
+        imageContainer.appendChild(imgElement);
+      }
     }
 
-    if (images.length === 0) {
-      imageContainer.textContent = 'No more images';
+    const remainingImages = imageContainer.querySelectorAll('img');
+
+    if (remainingImages.length === 0) {
+      const noImageText = document.createElement('p');
+      noImageText.textContent = 'No more images';
+      imageContainer.appendChild(noImageText);
     }
   };
 }
