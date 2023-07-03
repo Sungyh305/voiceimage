51d50be72a7aff5e79620740855d61907c7a16cd
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-07-03 16:03:19 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const API = window.SpeechRecognition || window.webkitSpeechRecognition;
 if (API) {
   const recognition = new API();
 
-  recognition.continuous = true; // 연속적인 음성 입력을 허용합니다.
-  recognition.lang = 'ko-KR'; // 인식할 언어를 설정합니다. 여기서는 한국어로 설정되어 있습니다.
+  recognition.continuous = true;
+  recognition.lang = 'ko-KR';
 
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
@@ -13,26 +13,41 @@ if (API) {
   let currentImageIndex = 0;
 
   button.addEventListener('click', () => {
-    recognition.start(); // 음성 인식을 시작합니다.
+    recognition.start();
     button.textContent = 'Listening...';
   });
 
   recognition.onresult = (event) => {
-    const transcript = event.results[0][0].transcript; // 음성 인식 결과에서 변환된 텍스트를 가져옵니다.
+    const transcript = event.results[0][0].transcript;
     speechResult.textContent = transcript;
     changeImage(transcript);
   };
 
   function changeImage(transcript) {
     const matchingImageIndex = imageUrls.findIndex(url => transcript.includes(url.replace('.jpg', '')));
-    // 음성 인식 결과와 이미지 파일명을 비교하여 일치하는 이미지의 인덱스를 찾습니다.
 
     if (matchingImageIndex !== -1) {
-      imageUrls.splice(matchingImageIndex, 1); // 일치하는 이미지를 배열에서 제거합니다.
-      const newImageIndex = currentImageIndex % imageUrls.length;
+      const newImageIndex = matchingImageIndex;
       mainImage.src = imageUrls[newImageIndex];
       currentImageIndex = newImageIndex;
-      // 일치하는 이미지를 현재 이미지로 설정하고, 이미지 인덱스를 업데이트합니다.
+
+      // 추가된 부분: 일치한 이미지에 동그라미 표시
+      const imageElements = document.querySelectorAll('.image-container img');
+      imageElements.forEach((img, index) => {
+        img.classList.toggle('highlighted', index === currentImageIndex);
+      });
+
+      setTimeout(() => {
+        // 3초 뒤에 이미지 변경
+        const nextImageIndex = (currentImageIndex + 1) % imageUrls.length;
+        mainImage.src = imageUrls[nextImageIndex];
+        currentImageIndex = nextImageIndex;
+
+        // 변경된 이미지에 동그라미 표시
+        imageElements.forEach((img, index) => {
+          img.classList.toggle('highlighted', index === currentImageIndex);
+        });
+      }, 3000);
     }
   }
 }
